686addda4ba45d5deee6061ef2c50684cbb63e38
roselyu <Roselyu@163.com>
2014-10-12 16:02:25 +0800

    info.h updated

--- kernel/include/info.h
+++ kernel/include/info.h
@@ -11,10 +11,10 @@
 #define INFO_H_
 
 	unsigned char kernel_name[] = "Explorer";
-	unsigned char kernel_version[] = "0.01 v14";
+	unsigned char kernel_version[] = "v14";
 	unsigned char author[] = "Rose<roselyu@163.com>,Hu wenjie(CN)<1@GhostBirdOS.org>";
 	unsigned char copyright[] = "Copyright 2013-2014 by Public Explorer Kernel Development Group.";
-	unsigned char base[] = "Public Explorer 0.0.1";
+	unsigned char base[] = "[This is Public Explorer.]";
 	unsigned char description[] = "This version of the memory pool design, improve memory utilization, because the memory management restructuring, temporarily canceled into paging mode.";
 
 #endif
