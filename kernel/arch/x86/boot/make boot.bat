e2925e459a0a40cdc905f1b4979267252939c03d
happybird1999 <happybird1999@outlook.com>
2015-04-19 08:16:03 +0800

    update

--- kernel/arch/x86/boot/make boot.bat
+++
@@ -1,6 +0,0 @@
-@echo Copyright 2013-2014 Ghost Bird OS Developer.All rights reserved. 
-@echo Ghost Bird OS C programme version - boot.
-:make
-	@pause
-	@nasm "boot(floppy disk).asm" -o "boot(floppy disk).bin"
-	@goto make
