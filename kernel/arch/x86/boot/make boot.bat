c83e1beea21c874e1640263ae9d6e6c1c185187f
happybird1999 <happybird1999@outlook.com>
2015-04-19 10:54:41 +0800

    内核源代码所在

--- kernel/arch/x86/boot/make boot.bat
+++
@@ -1,6 +0,0 @@
-@echo Copyright 2013-2015 Ghost Bird OS Developers.All rights reserved. 
-@echo Ghost Bird OS C programme version - boot.
-:make
-	@pause
-	@nasm "boot(floppy disk).asm" -o "boot(floppy disk).bin"
-	@goto make
